2938d5e7d2a98423711045ef48d86180dc4f52a1
david <a.davidromero@icloud.com>
2024-11-12 16:10:04 -0500

    Made basic user attribute input view, will add logic soon, and information bubbles explain why we need certain information

--- GolfSwingTrainer/Views/Start Dialog/UserAttributesInputView.swift
+++ GolfSwingTrainer/Views/Start Dialog/UserAttributesInputView.swift
@@ -8,115 +8,106 @@
 import SwiftUI
 
 struct UserAttributesInputView: View {
-    @State private var height: Double = 170 // Default in cm
-    @State private var weight: Double = 70 // Default in kg
-    @State private var age: Int = 30
-    @State private var gender: String = "Male"
+    @State private var height: Int = 170 // Default in cm
+    @State private var weight: Int = 70 // Default in kg
+    @State private var birthDate = Date.now
+    @State private var gender: String = "Other"
     @State private var dominantHand: String = "Right"
-    
+    @State private var preferredMeasurement: String = "Metric"
+    private let heights = 120...220
+    private let weights = 40...150
     private let genders = ["Male", "Female", "Non-Binary" ,"Other"]
     private let dominantHands = ["Right", "Left"]
+    private let preferredMeasurements = ["Imperial","Metric"]
     var body: some View {
         NavigationStack{
-            Text("Your Physical Attributes")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.top)
-            
-            VStack(alignment: .leading, spacing: 20) {
-                Section(String(localized: "Height")+String(localized: "(cm)")){
-                    
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "ruler")
-                            Slider(value: $height, in: 120...220, step: 1)
-                                .accentColor(.green)
-                            Text("\(Int(height)) cm")
-                                .foregroundColor(.secondary)
-                        }
-                    }.padding(.horizontal)
-                    
-                    
-                }.font(.headline).fontWeight(.semibold)
-
+            Form{
+                //Decription
+                Section(header: Text("Why do we need this?")){
+                    Text("TODO: ").font(.subheadline).fontWeight(.thin)
+                }
                 
-                Section(String(localized: "Weight")+String(localized: "(kg)")){
-                    // Weight Picker
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "scalemass")
-                            Slider(value: $weight, in: 40...150, step: 1)
-                                .accentColor(.blue)
-                            Text("\(Int(weight)) kg")
-                                .foregroundColor(.secondary)
-                        }
-                    }.padding(.horizontal)
-                }.font(.headline).fontWeight(.semibold)
-                
-                
-                // Age Picker
-                Section("Age"){
-                    VStack(alignment: .leading) {
-                        Picker("", selection: $age) {
-                            ForEach(10...100, id: \.self){ age in
-                                Text("\(age)")
+                //Input form
+                Section(header: Text("Enter your physical attributes")){
+                    
+                    //Preferred Measurment
+                    HStack(){
+                        Picker("Measurments", selection: $preferredMeasurement) {
+                            ForEach(preferredMeasurements, id: \.self) { measurement in
+                                Text(measurement)
                             }
-                            
-                        }.pickerStyle(.menu)
-                            .padding(.horizontal)
+                        }.pickerStyle(.segmented)
                     }
-                }.font(.headline).fontWeight(.semibold)
-                
-                // Gender Picker
-                Section("Gender"){
-                    VStack(alignment: .leading) {
-                        Text("Gender")
-                            .font(.headline)
-                        Picker("Select Gender", selection: $gender) {
+                    
+                    
+                    //Height
+                    HStack {
+                        Image(systemName: "ruler")
+                        Picker(String(localized: "Height")+String(localized: " (cm)"), selection: $height){
+                            ForEach(heights, id: \.self) { height in
+                                Text("\(height)cm").tag(height) // Display each height option
+                            }
+                        }.pickerStyle(.automatic)
+                    }.font(.headline).fontWeight(.semibold)
+
+                    //Weight
+                    HStack{
+                        Image(systemName: "scalemass")
+                        Picker(String(localized: "Weight")+String(localized: " (kg)"), selection: $weight){
+                            ForEach(weights, id: \.self ){ value in
+                                Text("\(value) kg").tag(value) // Display each height option
+                            }
+                        }
+                    }.font(.headline).fontWeight(.semibold)
+                    
+                    //Age/Birthday
+                    HStack{
+                        Image(systemName: "birthday.cake")
+                        Text(String(localized: "Birthday"))
+                        DatePicker(selection: $birthDate, in: ...Date.now, displayedComponents: .date){}
+                    }.font(.headline).fontWeight(.semibold)
+                    
+                    // Gender Picker
+                    HStack{
+                        Image(systemName: "person.fill.questionmark")
+                        Picker("Gender", selection: $gender) {
                             ForEach(genders, id: \.self) { gender in
                                 Text(gender)
                             }
                         }
-                        .pickerStyle(SegmentedPickerStyle())
-                        .padding(.horizontal)
-                    }
-                }
-                
-                // Dominant Hand Picker
-                Section("Dominant Hand"){
-                    VStack(alignment: .leading) {
+                        .pickerStyle(.automatic)
+                        
+                    }.font(.headline).fontWeight(.semibold)
+                    
+                    
+                    // Dominant Hand Picker
+                    HStack {
+                        Image(systemName: "hand.wave")
+                        Text(String(localized: "Dominant Hand"))
                         Picker("Select Dominant Hand", selection: $dominantHand) {
                             ForEach(dominantHands, id: \.self) { hand in
                                 Text(hand)
                             }
                         }
                         .pickerStyle(SegmentedPickerStyle())
-                        .padding(.horizontal)
-                    }
-                }.font(.headline).fontWeight(.semibold)
-                
-                
-                // Submit Button
-               /* Button(action: submit) {
-                    Text("Save Attributes")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                    }.font(.headline).fontWeight(.semibold)
+                    
                 }
-                .padding(.horizontal)
-                .padding(.top, 20)*/
-            }
-            .padding(.vertical)
+                Button(action: {
+                    //TODO:
+                }, label: {
+                    HStack(alignment: .lastTextBaseline){
+                        
+                        Text("Continue")
+                        Image(systemName: "arrow.right")
+                    }
+                })
+            }.navigationTitle("Your physical attributes")
+            
         }
     }
-    /*private func submit() {
-        print("User Attributes Saved")
-        // Here you would save to a model or submit to a server
-    }*/
 }
+
 
 
 #Preview {
