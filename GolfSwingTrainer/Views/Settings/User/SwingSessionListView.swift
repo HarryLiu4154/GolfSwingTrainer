3f0767a8b1ebfe1d179fad881a5460a6cb685116
Harry Liu <harryliu4154@hotmail.com>
2024-12-03 22:48:34 -0500

    functions for data analysis

--- GolfSwingTrainer/Views/Settings/User/SwingSessionListView.swift
+++ GolfSwingTrainer/Views/Settings/User/SwingSessionListView.swift
@@ -9,6 +9,46 @@ import Foundation
 
 struct SwingSessionListView: View {
     @EnvironmentObject var swingSessionViewModel: SwingSessionViewModel
+    
+    private func calculateAngle(from dataList: [[String: Double]]) -> Double? {
+        let angles = dataList.compactMap { data -> Double? in
+            guard let x = data["x"], let y = data["y"], let z = data["z"] else {
+                return nil
+            }
+            return sqrt(x * x + y * y + z * z)
+        }
+        
+        guard !angles.isEmpty else {
+            return nil // Return nil if no valid angles were calculated
+        }
+        
+        let total = angles.reduce(0, +)
+        return radTodeg(total / Double(angles.count))
+    }
+    
+    func radTodeg(_ number: Double) -> Double {
+        return number * 180 / .pi
+    }
+                           
+    private func calculateVelocity(from accelerationData: [[String: Double]]) -> Double? {
+        var velocityX = 0.0
+        var velocityY = 0.0
+        var velocityZ = 0.0
+        var deltaTime: Double = 0.02 // 50Hz or 0.02 seconds
+
+        for data in accelerationData {
+            guard let ax = data["x"], let ay = data["y"], let az = data["z"] else {
+                return nil // Invalid data
+            }
+            velocityX += ax * deltaTime
+            velocityY += ay * deltaTime
+            velocityZ += az * deltaTime
+        }
+
+        // Calculate the magnitude of the velocity vector
+        let velocity = sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ)
+        return velocity
+    }
 
     var body: some View {
         NavigationStack {
@@ -18,7 +58,11 @@ struct SwingSessionListView: View {
                         VStack(alignment: .leading) {
                             Text("Date: \(session.date.formatted(date: .abbreviated, time: .shortened))")
                                 .font(.headline)
-                            Text("\(session.rotationData.count) Rotation Samples, \(session.accelerationData.count) Acceleration Samples")
+//                            Text("\(session.rotationData.count) Rotation Samples, \(session.accelerationData.count) Acceleration Samples")
+//                                .font(.subheadline)
+//                                .foregroundColor(.gray)
+                            
+                            Text("Rotation: \(String(describing: calculateAngle(from: session.rotationData))) Degrees,\nSpeed: \(String(describing: calculateVelocity(from: session.accelerationData))) km/h")
                                 .font(.subheadline)
                                 .foregroundColor(.gray)
                         }
